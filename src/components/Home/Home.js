1bbb2fadd594f30a807658fa89afbccce9647801
sakshi-uplap <88712711+sakshi-uplap@users.noreply.github.com>
2021-10-14 15:45:22 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,25 @@
-import React, { useState, useCallback, useContext } from "react";
-import "./Home.scss";
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/banner.jpg";
+import "../Home/Home.css";
 import { myContext } from "../Context/Context";
-import Banner from "../assets/banner.jpg";
-import{Link} from "react-router-dom";
-
 
 function Home() {
-    return (
-        <div className="home" style={{backgroundImage: `url(${Banner})`}}>
-           <div className="headerConatiner">
-               <h2> COOK-BOOK</h2>
-               <p>Cook awesome recipes without hesitation! </p>
+  return (
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+      <h2> COOK-BOOK</h2>
+               <p>Cook awesome recipes</p>
+               <p>without hesitation! </p>
                 <Link to="/recipes">
                     <button>Recipes</button>
                 </Link>
                <Link to="/categories">
                     <button>Category</button>
                </Link>
-           </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
